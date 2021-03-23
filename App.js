49ecf1b1a73489f016e914320846cd91148051ea
Alexandre Wotawa <awotawa@gmail.com>
2021-03-23 15:26:38 +0100

    added image picker

--- App.js
+++ App.js
@@ -4,6 +4,18 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
+
   return (
     <View style={styles.container}>
       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
@@ -12,8 +24,7 @@ export default function App() {
         To share a photo from your phone with a friend, just press the button below!
       </Text>
 
-      <TouchableOpacity
-        onPress={() => alert('Hello, world!')} style={styles.button}>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
     </View>
